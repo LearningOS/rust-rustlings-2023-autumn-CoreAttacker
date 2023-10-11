a460075d4d287d19ab1f711d497f98073fa20db5
CoreAttacker <97329851+CoreAttacker@users.noreply.github.com>
2023-10-11 07:38:09 +0000

    update

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,13 +6,16 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
-fn main() {
+fn main() { //在同一个作用域内，对同一个变量 x 创建了两个可变引用 y 和 z。这违反了 Rust 的借用规则，
+    //因为 Rust 不允许同一个作用域内存在多个可变引用指向同一个值，以防止数据竞争和内存不安全。
+    //普通引用是可以同时存在多个的
     let mut x = 100;
-    let y = &mut x;
+    {
+        let y = &mut x;
+        *y += 100;
+    }
     let z = &mut x;
-    *y += 100;
     *z += 1000;
     assert_eq!(x, 1200);
 }
